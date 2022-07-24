88f623f5158ab46980dab27b1a587db067bbfe49
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-07-24 22:49:01 +0700

    slider for highlight

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,4 @@
-// slider 
-var sliderMain = document.getElementById('slider-main')
-var item = sliderMain.getElementsByClassName('item')
-function next() {
-    sliderMain.append(item[0]);
-}
-function prev() {
-    sliderMain.prepend(item[item.length - 1]);
-}
-
-// mobile navbar 
+// mobile navbar
 const mobileMenu = document.querySelector(".btn-mobile-navbar");
 const linkNavbar = document.querySelector(".mobile-navbar");
 
@@ -18,3 +8,42 @@ mobileMenu.addEventListener("click", function () {
 linkNavbar.addEventListener("click", function () {
   linkNavbar.classList.remove("open");
 });
+
+// slider
+const container = document.querySelector(".highlight-works");
+const slider = document.querySelector(".slider-wrap");
+
+let isPressedDown = false;
+let cursorXSpace;
+
+container.addEventListener("mousedown", (e) => {
+  isPressedDown = true;
+  cursorXSpace = e.offsetX - slider.offsetLeft;
+  container.style.cursor = "grabbing";
+});
+
+container.addEventListener("mouseup", () => {
+  container.style.cursor = "grab";
+});
+
+window.addEventListener("mouseup", () => {
+  isPressedDown = false;
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isPressedDown) return;
+  e.preventDefault();
+  slider.style.left = `${e.offsetX - cursorXSpace}px`;
+  boundslider();
+});
+
+function boundslider() {
+  const container_rect = container.getBoundingClientRect();
+  const slider_rect = slider.getBoundingClientRect();
+
+  return parseInt(slider.style.left) > 0
+    ? (slider.style.left = 0)
+    : slider_rect.right < container_rect.right
+    ? (slider.style.left = `-${slider_rect.width - container_rect.width}px`)
+    : "";
+}
